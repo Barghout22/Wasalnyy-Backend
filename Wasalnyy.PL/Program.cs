8ebadb09dd4282635d001c40938cdaae3a034f50
Maryam Ahmed <msoliman@stingrayconsultation.com>
2025-11-14 21:55:07 +0200

    link angular to backend(authentication)

--- Wasalnyy.PL/Program.cs
+++ Wasalnyy.PL/Program.cs
@@ -39,7 +39,15 @@ namespace Wasalnyy.PL
 			builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
-            var app = builder.Build();
+			builder.Services.AddCors(options =>
+			{
+				options.AddPolicy("AllowAngular",
+					policy => policy.WithOrigins("http://localhost:4200")
+									.AllowAnyHeader()
+									.AllowAnyMethod());
+			});
+
+			var app = builder.Build();
 			using (var scope = app.Services.CreateScope())
 			{
 				var services = scope.ServiceProvider;
@@ -58,7 +66,8 @@ namespace Wasalnyy.PL
             app.UseAuthentication();
             app.UseAuthorization();
             app.MapHub<TripHub>("/ride");
-            app.MapControllers();
+			app.UseCors("AllowAngular");
+			app.MapControllers();
             app.Run();
         }
     }
